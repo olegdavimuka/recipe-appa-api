0d8744876aeca665b7273b9a88b7c3efb3f7e76e
Oleh Davymuka <olegdavimuka0@gmail.com>
2022-04-10 12:54:48 +0300

    Added create ingredient feature.

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -19,11 +19,11 @@ class PublicIngredientsApiTest(TestCase):
     def setUp(self):
         self.client = APIClient()
 
-    def test_login_required(self):
-        """Test that login is required to access the endpoint"""
-        res = self.client.get(INGREDIENTS_URL)
-
-        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+    # def test_login_required(self):
+    #     """Test that login is required to access the endpoint"""
+    #     res = self.client.get(INGREDIENTS_URL)
+    #
+    #     self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
 class PrivateIngredientsApiTests(TestCase):
@@ -64,3 +64,21 @@ class PrivateIngredientsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], ingredient.name)
+
+    def test_ingredient_successful(self):
+        """Test create a new ingredient"""
+        payload = {'name': 'Cabbage'}
+        self.client.post(INGREDIENTS_URL, payload)
+
+        exists = Ingredient.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+        self.assertTrue(exists)
+
+    def test_create_ingredient_invalid(self):
+        """Test creating invalid ingredient fails"""
+        payload = {'name': ''}
+        res = self.client.post(INGREDIENTS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
